1925a42d8e9f9ff73a4cdeee4234aaded87e0a6f
FINN-5 <bmd040510@gmail.com>
2024-03-04 23:20:37 +0700

    lan 1

---
+++ baitaplonC#/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace baitaplonC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 0);
+            button1.Location = new Point(204, 34);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 63);
+            button1.TabIndex = 0;
+            button1.Text = "Đổi Mật Khẩu ";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 0);
+            button3.Location = new Point(204, 127);
+            button3.Name = "button3";
+            button3.Size = new Size(173, 63);
+            button3.TabIndex = 2;
+            button3.Text = "Đăng Xuất";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Location = new Point(39, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(312, 45);
+            button2.TabIndex = 3;
+            button2.Text = "Thông Tin Cá Nhân";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.Location = new Point(39, 288);
+            button4.Name = "button4";
+            button4.Size = new Size(312, 45);
+            button4.TabIndex = 4;
+            button4.Text = "Quản Lý Tài Khoản";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaption;
+            button5.Location = new Point(39, 352);
+            button5.Name = "button5";
+            button5.Size = new Size(312, 45);
+            button5.TabIndex = 5;
+            button5.Text = "Quản Lý Hàng Hóa";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaption;
+            button6.Location = new Point(39, 415);
+            button6.Name = "button6";
+            button6.Size = new Size(312, 45);
+            button6.TabIndex = 6;
+            button6.Text = "Quản Lý Giá Nhập";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveCaption;
+            button8.Location = new Point(39, 627);
+            button8.Name = "button8";
+            button8.Size = new Size(312, 45);
+            button8.TabIndex = 9;
+            button8.Text = "Xuất Kho";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveCaption;
+            button9.Location = new Point(39, 555);
+            button9.Name = "button9";
+            button9.Size = new Size(312, 45);
+            button9.TabIndex = 8;
+            button9.Text = "Nhập Kho";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaption;
+            button10.Location = new Point(39, 482);
+            button10.Name = "button10";
+            button10.Size = new Size(312, 45);
+            button10.TabIndex = 7;
+            button10.Text = "Quản Lý Giá Xuất";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.z5212876671755_bbb3e0cae80caaa7587d208235d2ee32;
+            ClientSize = new Size(1310, 762);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Phần Mềm Quản Lý Kho Hàng";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+    }
+}
---
+++ baitaplonC#/Form2.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace baitaplonC_
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(187, 72);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(337, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(187, 138);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(337, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Tài Khoản ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Mật Khẩu";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(187, 283);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 35);
+            button1.TabIndex = 4;
+            button1.Text = "✅ Đăng Nhập";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(371, 283);
+            button2.Name = "button2";
+            button2.Size = new Size(153, 35);
+            button2.TabIndex = 5;
+            button2.Text = "📤 Thoát";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Microsoft Sans Serif", 8.25F);
+            checkBox1.Location = new Point(187, 189);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(123, 21);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Hiện Mật Khẩu";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources._0b549b82e4d3c640ead42f9b66697d92;
+            ClientSize = new Size(559, 351);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form2";
+            Text = "Đăng Nhập";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+    }
+}
