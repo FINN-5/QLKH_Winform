fb65f989310bf04f642ec90ae7579d21d55da406
KillerBee244 <vuvandien24092003@gmail.com>
2024-03-09 20:05:58 +0700

    lan10

---
+++ baitaplonC#/nhaphang.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitaplonC_
+{
+    public partial class nhaphang : Form
+    {
+        public nhaphang()
+        {
+            InitializeComponent();
+        }
+
+        private void nhaphang_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private Form currentchildform;
+        private void openchildform(Form childform)
+        {
+            if (currentchildform != null)
+            {
+                currentchildform.Close();
+                //neu form tren da khoi tao roi thi dong lai
+            }
+            currentchildform = childform;
+            childform.TopLevel = false;
+            //khi show form nay thi can thuc hien len form cha
+            childform.FormBorderStyle = FormBorderStyle.None;
+            //bo hop thoai cua from
+            childform.Dock = DockStyle.Fill;
+            panel1.Controls.Add(childform);
+            panel1.Tag = childform;
+            childform.BringToFront();
+            childform.Show();
+        }
+        //su kien khi an nut sua
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openchildform(new suanhap());
+        }
+
+       
+    }
+
+}
+
---
+++ baitaplonC#/suanhap.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace baitaplonC_
+{
+    partial class suanhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(111, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(180, 46);
+            label1.TabIndex = 0;
+            label1.Text = "Hàng Hóa";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(111, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(167, 46);
+            label2.TabIndex = 1;
+            label2.Text = "Giá Nhập";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(383, 92);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(427, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(383, 172);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(427, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.Location = new Point(271, 293);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 60);
+            button1.TabIndex = 4;
+            button1.Text = "Lưu";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button2.Location = new Point(493, 293);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 60);
+            button2.TabIndex = 5;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // suanhap
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(863, 437);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "suanhap";
+            Text = "Sửa Thông Tin Giá Nhập";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
