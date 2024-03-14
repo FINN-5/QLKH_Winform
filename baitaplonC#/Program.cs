2e80b4070d79a805dec1386c7c3ef3a8cff54cd8
Cakesoup <144873675+Cakesoup@users.noreply.github.com>
2024-03-14 17:36:40 +0700

    Update Program.cs

--- baitaplonC#/Program.cs
+++ baitaplonC#/Program.cs
@@ -14,11 +14,7 @@ namespace baitaplonC_
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
             Application.Run(new QuanLyTaiKhoan());
-=======
-            Application.Run(new Dangnhap());
->>>>>>> 8ce98215e5edf7d6fa303eea8799a752ce8b96a3
         }
     }
 }
